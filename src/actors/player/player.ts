1fbd55ac482264bec10088a90ee3d2764ca9643c
localyost <yostpa@gmail.com>
2021-12-20 19:25:34 +0100

    initial commit

--- src/actors/player/player.ts
+++ src/actors/player/player.ts
@@ -1,7 +1,12 @@
-import { Actor, Color, vec } from 'excalibur';
-import { Resources } from '../../resources';
+import {Actor, Color, Engine, vec} from 'excalibur';
+import {Resources} from '../../resources';
+import * as ex from 'excalibur';
+import Config from "../../config";
+
 
 export class Player extends Actor {
+
+  private moveDistance = 6;
   constructor() {
     super({
       pos: vec(150, 150),
@@ -11,7 +16,42 @@ export class Player extends Actor {
     });
   }
 
-  onInitialize() {
+  onInitialize(engine:Engine) {
     this.graphics.use(Resources.Sword.toSprite());
+    engine.input.keyboard.on('hold', (event) => this.handleKeyEvent(engine, event));
+    engine.input.keyboard.on('release', (evt: ex.Input.KeyEvent) => {
+      if(evt.key !== ex.Input.Keys.Space) {
+        this.vel = ex.Vector.Zero.clone()
+      }
+    });
   }
+
+  handleKeyEvent = (engine: ex.Engine, evt: ex.Input.KeyEvent) => {
+    let dir = ex.Vector.Zero.clone();
+    if (evt.key === ex.Input.Keys.Space) {
+      if (this.vel.x !== 0 || this.vel.y !== 0) {
+        dir = this.vel.normalize();
+      }
+    }
+    // Some keys do the same thing
+    if (evt.key === ex.Input.Keys.W) {
+      dir.y += -1;
+    }
+    if (evt.key === ex.Input.Keys.A) {
+      dir.x += -1;
+    }
+
+    if (evt.key === ex.Input.Keys.D) {
+      dir.x += 1;
+    }
+
+    if (evt.key ===  ex.Input.Keys.S) {
+      dir.y += 1;
+    }
+
+    if (dir.x !== 0 || dir.y !== 0) {
+      this.vel = dir.normalize().scale(Config.playerSpeed);
+    }
+  }
+
 }
